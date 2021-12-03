93bb38991c3719d93f4f0c870158241170d844d3
August2111 <Info@FlapsOnline.de>
2021-12-03 22:39:59 +0100

    build-image.py switch to CH070 as standard machine
    Byside use the working directory to run the build, not a hardcoded folder

--- build-image.py
+++ build-image.py
@@ -12,15 +12,15 @@ my_env = os.environ.copy()
 cwd = os.getcwd()
 
 
-# my_env['MACHINE'] =  'openvario-7-CH070'
+my_env['MACHINE'] =  'openvario-7-CH070'
 # my_env['MACHINE'] =  'openvario-57-lvds'
 # my_env['MACHINE'] =  'openvario-43-rgb'
-my_env['MACHINE'] =  'openvario-7-AM070-DS2'
+# my_env['MACHINE'] =  'openvario-7-AM070-DS2'
 
-myprocess = subprocess.Popen(['/opt/openvario/scripts/build-ov.sh'], env = my_env, cwd=cwd+'/poky', shell = False)
-# myprocess = subprocess.Popen(['/opt/openvario/scripts/build-u-boot.sh'], env = my_env, cwd=cwd+'/poky', shell = False)
+myprocess = subprocess.Popen(['build-scripts/build-lua.sh'], env = my_env, cwd=cwd+'/poky', shell = False)
+# myprocess = subprocess.Popen(['scripts/build-ov.sh'], env = my_env, cwd=cwd+'/poky', shell = False)
+# myprocess = subprocess.Popen(['build-scripts/build-u-boot.sh'], env = my_env, cwd=cwd+'/poky', shell = False)
 myprocess.wait()
-
 
 # ========================================================================================================================
 # ========================================================================================================================
