7d0fcfb749a982979d32bd25552ab773a116b5ff
August2111 <Info@FlapsOnline.de>
2021-12-12 20:48:15 +0100

    scripts/build-ov.sh - call openvario-image as single shot alone

--- scripts/build-ov.sh
+++ scripts/build-ov.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
+cd poky
 TEMPLATECONF=meta-openvario/conf source oe-init-build-env
 
-# bitbake openvario-image -c listtasks
-# bitbake openvario-image -c fetchall
-# bitbake openvario-image -g -u depexp
-
-# bitbake -c cleanall openvario-image
 bitbake openvario-image
 
