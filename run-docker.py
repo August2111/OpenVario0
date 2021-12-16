fab2d094aae1a9da2c031ab10c93d0bf0890b589
August2111 <Info@FlapsOnline.de>
2021-12-16 17:23:53 +0100

    run-docker.py - cleanup

--- run-docker.py
+++ run-docker.py
@@ -19,32 +19,25 @@ print(' Path-Variable is: ', cwd, '!!!!!!!!!!!!!!')
 dockerfile = 'scripts/Dockerfile'
 
 # test image detection
-# fileout = ''
-# stdout=out,
 out = open('docker-out.txt', 'w')
-# myprocess = subprocess.Popen(['docker', 'image', 'ls', image], env = my_env, cwd = cwd, stdout = out, shell = False)
-# myprocess = subprocess.Popen(['docker', 'image', 'ls', '-q', '--filter', "reference=*/*:latest"], env = my_env, cwd = cwd, stdout = out, shell = False)
 myprocess = subprocess.Popen(['docker', 'image', 'ls', '-q', image], env = my_env, cwd = cwd, stdout = out, shell = False)
-# myprocess = subprocess.Popen(['docker', 'image', 'ls' ,'--filter', '"dangling=true"', image], env = my_env, cwd = cwd, stdout = out, shell = False)
-# myprocess = subprocess.Popen(['docker', 'image', 'ls' ,'--filter', '"dangling=true"', '>', 'docker-ls.txt'], env = my_env, cwd = cwd, shell = False)
 myprocess.wait()
 out.close()
 
 out = open('docker-out.txt', 'r')
 image_id = out.read()
 out.close()
-print('image: ', image,', image-id = ', image_id, ' --- ', len(image_id))
-with_docker_build = len(image_id) > 0
 
-#    with_docker_build = True
+print('image: ', image,', image-id = ', image_id, ' --- ', len(image_id))
+
+if len(image_id) > 0:  # with_docker_build:
+    myprocess = subprocess.Popen(['docker', 'build', '--file', dockerfile, '-t', image, './'], env = my_env, cwd = cwd, shell = False)
+    myprocess.wait()
+
+target_dir = '/opt/openvario'
 if sys.platform.startswith('win'):
     # is win, but this is very preliminary
-    target_dir = '/opt/openvario'
     target_dir = '/home/pokyuser'  # overwrite previous!
-
-    if with_docker_build:
-        myprocess = subprocess.Popen(['docker', 'build', '--file', dockerfile, '-t', image, './'], env = my_env, cwd = cwd, shell = False)
-        myprocess.wait()
     # myprocess = subprocess.Popen(['docker', 'run', '-u "pokyuser"' , '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image
     # myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image
     myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image
@@ -52,13 +45,12 @@ if sys.platform.startswith('win'):
     # , '/opt/openvario/build-image.py'  ], env = my_env, cwd = cwd, shell = False)
 else:
     # is linux:
-    target_dir = '/opt/openvario'
     # target_dir = '/home/pokyuser'
-
     if with_docker_build:
         myprocess = subprocess.Popen(['docker', 'build', '--file', dockerfile, '-t', image, './'], env = my_env, cwd = cwd, shell = False)
         myprocess.wait()
-    myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image
+
+myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image
        , '--workdir=' + target_dir], env = my_env, cwd = cwd, shell = False)
 myprocess.wait()
 
