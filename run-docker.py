3b2d9f29f194ef19b6be7a04dfa426d492c732ad
August2111 <Info@FlapsOnline.de>
2021-12-17 10:34:40 +0100

    run-docker.py - cleanup!

--- run-docker.py
+++ run-docker.py
@@ -3,24 +3,15 @@
 import os, shutil, subprocess, sys
 
 branch  = 'hardknott'
+# branch  = 'honister'
 
 print('python run-docker.py with Branch ', branch)
 print('==========================================')
-my_env = os.environ.copy()
-if sys.platform.startswith('win'):    # is win
-    cwd = os.getcwd().replace('\\', '/')
-else:                                 # is linux:
-    cwd = os.getcwd()
 
-image = 'openvario/' + branch + ':latest'
-
-print(' Path-Variable is: ', cwd, '!!!!!!!!!!!!!!')
-
-dockerfile = 'scripts/Dockerfile'
-
+def DockerImageAvailable(image_name):
 # test image detection
 out = open('docker-out.txt', 'w')
-myprocess = subprocess.Popen(['docker', 'image', 'ls', '-q', image], env = my_env, cwd = cwd, stdout = out, shell = False)
+myprocess = subprocess.Popen(['docker', 'image_name', 'ls', '-q', image_name], env = my_env, cwd = cwd, stdout = out, shell = False)
 myprocess.wait()
 out.close()
 
@@ -28,30 +19,51 @@ out = open('docker-out.txt', 'r')
 image_id = out.read()
 out.close()
 
-print('image: ', image,', image-id = ', image_id, ' --- ', len(image_id))
+print('image_name: ', image_name,', image-id = ', image_id, ' --- ', len(image_id))
+
+#==============================================================
+#==============================================================
+#==============================================================
+my_env = os.environ.copy()
+if sys.platform.startswith('win'):    # is win
+    cwd = os.getcwd().replace('\\', '/')
+else:                                 # is linux:
+    cwd = os.getcwd()
+
+image_name = 'openvario/' + branch + ':latest'
+
+print(' Path-Variable is: ', cwd, '!!!!!!!!!!!!!!')
+
+dockerfile = 'scripts/Dockerfile'
+image_id = 
 
 if len(image_id) > 0:  # with_docker_build:
-    myprocess = subprocess.Popen(['docker', 'build', '--file', dockerfile, '-t', image, './'], env = my_env, cwd = cwd, shell = False)
+    myprocess = subprocess.Popen(['docker', 'build', '--file', dockerfile, '-t', image_name, './'], env = my_env, cwd = cwd, shell = False)
     myprocess.wait()
 
+
 target_dir = '/opt/openvario'
+set_target_dir = ''
+build_cmd  = target_dir + 'build-image.py'
 if sys.platform.startswith('win'):
     # is win, but this is very preliminary
     target_dir = '/home/pokyuser'  # overwrite previous!
-    # myprocess = subprocess.Popen(['docker', 'run', '-u "pokyuser"' , '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image
-    # myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image
-    myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image
-    , './build-image.py'  ], env = my_env, cwd = cwd, shell = False)
-    # , '/opt/openvario/build-image.py'  ], env = my_env, cwd = cwd, shell = False)
+    ### delete:    # myprocess = subprocess.Popen(['docker', 'run', '-u "pokyuser"' , '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image_name
+    ### delete:    # myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image_name
+    ### delete:    myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image_name
+    ### delete:    , './build-image.py'  ], env = my_env, cwd = cwd, shell = False)
+    ### delete:    # , '/opt/openvario/build-image.py'  ], env = my_env, cwd = cwd, shell = False)
 else:
     # is linux:
     # target_dir = '/home/pokyuser'
-    if with_docker_build:
-        myprocess = subprocess.Popen(['docker', 'build', '--file', dockerfile, '-t', image, './'], env = my_env, cwd = cwd, shell = False)
-        myprocess.wait()
+    set_target_dir = '--workdir=' + target_dir
+    ### delete:    myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image_name
+    ### delete:    , './build-image.py'  ], env = my_env, cwd = cwd, shell = False)
 
-myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image
-       , '--workdir=' + target_dir], env = my_env, cwd = cwd, shell = False)
+### delete:myprocess.wait()
+
+myprocess = subprocess.Popen(['docker', 'run', '--rm', '--mount', 'type=bind,source='+ cwd + ',target=' + target_dir, '-it', image_name
+       , set_target_dir, build_cmd], env = my_env, cwd = cwd, shell = False)
 myprocess.wait()
 
 print('Finish: run-docker.py')
